6bedb2d739a41ce7598e0775dc747cd1e3ee28e8
Ivan Echegaray <ivan.echegaray@outlook.com>
2021-04-07 17:17:30 -0500

    add module

---
+++ examples/test/main.tf
@@ -0,0 +1,31 @@
+module "this" {
+  source = "../../"
+
+  name           = "titan-dev-test"
+  artifacts_type = "NO_ARTIFACTS"
+  service_role   = module.iam-role.values.arn
+  source_type    = "NO_SOURCE"
+  source_buildspec = file("buildspec.yml")
+
+  tags = {
+    Project = "titan"
+  }
+}
+
+# dependencies
+module "iam-role" {
+  source  = "bancoripleyperu/iam-role/aws"
+  version = "0.0.3"
+
+  name         = "titan-dev-test"
+  aws_services = ["codebuild.amazonaws.com"]
+
+  tags = {
+    Project = "titan"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = module.iam-role.values.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+}
